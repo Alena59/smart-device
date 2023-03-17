e99220d248bb80375043107c87572af4c2c0ef38
Alena59 <asdalena59@gmail.com>
2023-03-18 02:57:56 +0300

    commit

--- source/js/modules/validation.js
+++ source/js/modules/validation.js
@@ -1,19 +1,18 @@
-const modalContainer = document.querySelector('[data-modal="feedback"]');
-const username = document.querySelector('#modal-input-name');
-const phone = document.querySelector('#modal-input-phone');
+const username = document.querySelector('.modal [name=username]');
+const phone = document.querySelector('.modal [name=phone]');
 const modalForm = document.querySelector('#modal-form');
 const checkbox = document.querySelector('#modal-agreement');
 
 function validateForm() {
-  if (modalContainer && username && phone && modalForm && checkbox) {
-    modalContainer.addEventListener('submit', function () {
+  if (username && phone && modalForm && checkbox) {
+    modalForm.addEventListener('submit', function () {
       if (username.value && phone.value && checkbox.checked) {
         localStorage.setItem('username', username.value);
         localStorage.setItem('phone', phone.value);
         modalForm.submit();
       }
-      modalForm.reset();
     });
+    modalForm.reset();
   }
 }
 
